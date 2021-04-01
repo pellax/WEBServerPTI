0a2885deff83eff7ab89d7e093b61bec2188593a
pellax <tuxisalive@gmail.com>
2021-04-01 20:06:25 +0200

    jueves

--- app.js
+++ app.js
@@ -3,10 +3,27 @@ const fs = require('fs')
 const app = express()
 const port = 32685
 
-app.get('/login',(req,res, next) => {
+//Importams el modulo JSON
+app.use(express.json());
 
+//Miramos si el fichero de usuarios existe y sino lo creamos 
+const usersFile = './users.json'
+if (!(fs.existsSync(usersFile)) {
+  fs.writeFileSync(usersFile,'{"users": []}');
+}
+
+
+app.get('/login',(req, res, next) => {
+
+  req.body.user 
+  req.body.password 
+  bool error = false;
+  
 
 })
+
+
+
 app.listen(port, () => {
   console.log(`PTI HTTP Server listening at http://localhost:${port}`)
 })
